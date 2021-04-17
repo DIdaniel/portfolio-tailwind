4f055e00c1ffc8b0c35a8e9832c3bf4cf9892a0d
dream_Faunus <cappu159@gmail.com>
2021-04-17 10:40:00 +0900

    portfolio with tailwindcss

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className="flex justify-between py-5 px-5 absolute w-full text-white">
+      <div className="logo">
+        <h2 className="text-3xl lg:text-5xl">
+          <Link to="/">DONG-IL</Link>
+        </h2>
+      </div>
+
+      <nav>
+        <ul className="flex">
+          <li className="mr-5">
+            <Link to="/">HOME</Link>
+          </li>
+          <li className="mr-5">
+            <Link to="/projects">PROJECTS</Link>
+          </li>
+          <li>
+            <Link 
+              to="/hire"
+              className="mr-5 bg-green-500 p-1 px-4 rounded-full font-semibold transition-all hover:bg-green-800">
+              HIRE
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/data.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    id: 1,
+    path: './images/milky.jpg',
+    desc: 'Milkyway'
+  },
+  {
+    id: 2,
+    path: './images/america.jpg',
+    desc: 'america'
+  },
+  {
+    id: 3,
+    path: './images/cube.jpg',
+    desc: 'cube'
+  },
+  {
+    id: 4,
+    path: './images/macbook.jpg',
+    desc: 'macbook'
+  },
+  {
+    id: 5,
+    path: './images/woman.jpg',
+    desc: 'woman'
+  },
+  {
+    id: 6,
+    path: './images/patrick.jpg',
+    desc: 'patrick'
+  },
+]
+
+export default data
